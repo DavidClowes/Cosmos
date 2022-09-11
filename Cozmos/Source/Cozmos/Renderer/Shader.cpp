13d244355d1bea11bccc7a977a5b53efe103e5f7
David Clowes <davidclowesdev@gmail.com>
2022-09-11 18:00:56 +1000

    Setup creating shader programs from one glsl file.

--- Cozmos/Source/Cozmos/Renderer/Shader.cpp
+++ Cozmos/Source/Cozmos/Renderer/Shader.cpp
@@ -6,16 +6,31 @@
 
 namespace Cozmos
 {
+	Shader* Shader::Create(const std::string& filepath)
+	{
+		switch (Renderer::GetAPI())
+		{
+			case RendererAPI::API::None:
+				COZ_CORE_ASSERT(false, "RendererAPI::None is not supported!");
+				return nullptr;
+
+			case RendererAPI::API::OpenGL:
+				return new OpenGLShader(filepath);
+		}
+
+		COZ_CORE_ASSERT(false, "Unknown RendererAPI!")
+			return nullptr;
+	}
 	Shader* Shader::Create(const std::string& vertexSrc, const std::string& fragmentSrc)
 	{
 		switch (Renderer::GetAPI())
 		{
-		case RendererAPI::API::None:
-			COZ_CORE_ASSERT(false, "RendererAPI::None is not supported!");
-			return nullptr;
+			case RendererAPI::API::None:
+				COZ_CORE_ASSERT(false, "RendererAPI::None is not supported!");
+				return nullptr;
 
-		case RendererAPI::API::OpenGL:
-			return new OpenGLShader(vertexSrc, fragmentSrc);
+			case RendererAPI::API::OpenGL:
+				return new OpenGLShader(vertexSrc, fragmentSrc);
 		}
 
 		COZ_CORE_ASSERT(false, "Unknown RendererAPI!")

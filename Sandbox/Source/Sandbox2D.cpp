9a47915dc6ab3ed74c4392afabdf14119b66cf78
David Clowes <davidclowesdev@gmail.com>
2022-11-06 21:18:00 +1100

    Setup 2D Renderer Transforms. Drawing Quads with position and scale.

--- Sandbox/Source/Sandbox2D.cpp
+++ Sandbox/Source/Sandbox2D.cpp
@@ -4,8 +4,6 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-#include "Platform/OpenGL/OpenGLShader.h"
-
 Sandbox2D::Sandbox2D()
 	: Layer("Sandbox2D"), m_CameraController(16.0f / 9.0f)
 {
@@ -31,11 +29,10 @@ void Sandbox2D::OnUpdate(Cozmos::Timestep ts)
 
 	Cozmos::Renderer2D::BeginScene(m_CameraController.GetCamera());
 	{
-		Cozmos::Renderer2D::DrawQuad({ 0.0f, 0.0f, }, { 1.0f, 1.0f }, { 0.8f, 0.2f, 0.3f, 1.0f });
+		Cozmos::Renderer2D::DrawQuad({ -1.0f, 0.0f, }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
+		Cozmos::Renderer2D::DrawQuad({ 0.5f, -0.5f, }, { 0.5f, 0.75f }, { 0.2f, 0.3f, 0.8f, 1.0f });
 	}
 	Cozmos::Renderer2D::EndScene();
-	// std::dynamic_pointer_cast<Cozmos::OpenGLShader>(m_FlatColorShader)->UploadUniformFloat4("u_Color", m_SquareColor);
-	// std::dynamic_pointer_cast<Cozmos::OpenGLShader>(m_FlatColorShader)->Bind();
 }
 
 void Sandbox2D::OnImGuiRender()

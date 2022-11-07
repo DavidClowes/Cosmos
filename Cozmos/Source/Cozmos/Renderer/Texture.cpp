da42ae2727d1501843786f878a82597e56b62f77
David Clowes <davidclowesdev@gmail.com>
2022-11-07 21:34:37 +1100

    Made it so a single shader is used for both textured mesh and colored mesh.

--- Cozmos/Source/Cozmos/Renderer/Texture.cpp
+++ Cozmos/Source/Cozmos/Renderer/Texture.cpp
@@ -6,6 +6,21 @@
 
 namespace Cozmos
 {
+	Ref<Texture2D> Texture2D::Create(uint32_t width, uint32_t height)
+	{
+		switch (Renderer::GetAPI())
+		{
+		case RendererAPI::API::None:
+			COZ_CORE_ASSERT(false, "RendererAPI::None is not supported!");
+			return nullptr;
+
+		case RendererAPI::API::OpenGL:
+			return CreateRef<OpenGLTexture2D>(width, height);
+		}
+
+		COZ_CORE_ASSERT(false, "Unknown RendererAPI!")
+			return nullptr;
+	}
 	Ref<Texture2D> Texture2D::Create(const std::string & path)
 	{
 		switch (Renderer::GetAPI())
@@ -15,7 +30,7 @@ namespace Cozmos
 			return nullptr;
 
 		case RendererAPI::API::OpenGL:
-			return std::make_shared<OpenGLTexture2D>(path);
+			return CreateRef<OpenGLTexture2D>(path);
 		}
 
 		COZ_CORE_ASSERT(false, "Unknown RendererAPI!")
